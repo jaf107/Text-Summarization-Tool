7fde6a5479af16599d7dcb49b6daa16490b452c8
jaf107 <48786629+jaf107@users.noreply.github.com>
2020-06-10 19:43:51 +0600

    TextRank initials
    Signed-off-by: jaf107 <48786629+jaf107@users.noreply.github.com>

--- src/TextRank/Main.java
+++ src/TextRank/Main.java
@@ -3,5 +3,21 @@ package TextRank;
 public class Main {
     public static void main(String[] args) {
 
+        /*
+
+        Steps:
+        1. Concatenate all the text .
+        2. Split the text into individual sentences .
+        3. Word Embedding for each and every sentence .
+        4. Similarity between sentence vectors -> Cosine similarity .
+        5. Similarity Matrix is converted to a graph where
+                - sentence          -> vertices .
+                - similarity scores -> edges .
+        6. Selection of N sentences .
+
+        */
+
+        SummaryTool mytool = new SummaryTool();
+
     }
 }
