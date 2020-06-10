fba12bc4fec820ad414ae8fbc31baabf97895fb0
jaf107 <48786629+jaf107@users.noreply.github.com>
2020-06-10 20:26:58 +0600

    Initialization of Functions
    Signed-off-by: jaf107 <48786629+jaf107@users.noreply.github.com>

--- src/TextRank/Main.java
+++ src/TextRank/Main.java
@@ -19,5 +19,6 @@ public class Main {
 
         SummaryTool mytool = new SummaryTool();
 
+        mytool.mainProcess();
     }
 }
--- src/TextRank/SummaryTool.java
+++ src/TextRank/SummaryTool.java
@@ -1,4 +1,42 @@
 package TextRank;
 
 public class SummaryTool {
+
+    void mainProcess()
+    {
+        int n = 7 ;// Random initialization
+        concatanateText();
+        textToSentence();
+        wordEmbedding();
+        cosineSimilarity();
+        matrixToGraph();
+        generateSummary( n );
+    }
+
+    void concatanateText()
+    {
+
+    }
+    void textToSentence()
+    {
+
+
+    }
+    void wordEmbedding()
+    {
+
+    }
+    void cosineSimilarity()
+    {
+
+    }
+    void matrixToGraph()
+    {
+
+    }
+    void generateSummary(int n)
+    {
+        // n is the number of selected sentences
+    }
+
 }
