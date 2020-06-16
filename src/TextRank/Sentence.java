a6a39adc66742b187ae1fb67e5111d6257bbeaa5
jaf107 <48786629+jaf107@users.noreply.github.com>
2020-06-16 09:53:33 +0600

    Sentence Class Initials
    Parameters of Sentence are finalised
    - text
    - Sentence length
    - Number of Words
    - Frequency -> TF-IDF
    - SCORE
    
    Signed-off-by: jaf107 <48786629+jaf107@users.noreply.github.com>

---
+++ src/TextRank/Sentence.java
@@ -0,0 +1,90 @@
+package TextRank;
+
+public class Sentence {
+    String text = "The name of my University is Dhaka University.";
+
+    // PARAMETERS
+    private int sentenencePosition;
+    private int frequency;                  // TF - IDF
+    private int numberOfWords;
+    private int sentenceLength;
+    private int numberOfProperNoun;         // Implementation later
+
+//    CONSTRUCTOR
+
+    Sentence()
+    {
+
+    }
+    Sentence(String sample,int sentenencePosition)
+    {
+        this.text = sample;
+        this.sentenencePosition = sentenencePosition;
+        this.sentenceLength = text.length();
+        this.numberOfWords = getNumberOfWords(sample);
+
+    }
+
+//    METHODS
+    void scoreSentence()
+    {
+
+    }
+
+
+
+
+//    GETTERS and SETTERS
+    public String getSample() {
+        return text;
+    }
+
+    public void setSample(String sample) {
+        this.text = sample;
+    }
+
+    public int getSentenencePosition() {
+        return sentenencePosition;
+    }
+
+    public void setSentenencePosition(int sentenencePosition) {
+        this.sentenencePosition = sentenencePosition;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+
+    public int getNumberOfWords(String s) {
+        int spaceCounter = 0;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ' ')
+                spaceCounter++;
+
+        }
+
+        return spaceCounter+1;
+    }
+
+    public void setNumberOfWords(int numberOfWords) {
+        this.numberOfWords = numberOfWords;
+    }
+
+    public int getSentenceLength() {
+        return sentenceLength;
+    }
+
+    public void setSentenceLength(int sentenceLength) {
+        this.sentenceLength = sentenceLength;
+    }
+
+    public int getNumberOfProperNoun() {
+        return numberOfProperNoun;
+    }
+
+    public void setNumberOfProperNoun(int numberOfProperNoun) {
+        this.numberOfProperNoun = numberOfProperNoun;
+    }
+}
