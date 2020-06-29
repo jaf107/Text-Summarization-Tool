3fea0dbbb9479d9ce272efa24668de8d9548e1ba
jaf107 <48786629+jaf107@users.noreply.github.com>
2020-06-29 20:27:51 +0600

    Sentence and Paragraph class initiated
    Signed-off-by: jaf107 <48786629+jaf107@users.noreply.github.com>

--- src/oldSummarization/Sentence.java
+++ src/oldSummarization/Sentence.java
@@ -1,4 +1,21 @@
 package oldSummarization;
 
 public class Sentence {
+    private int paragraphNumber ;
+    private int number;
+    private double score;
+    private int noOfWords;
+    private String value;
+    private String[] words;
+
+
+    Sentence(int number, String value, int paragraphNumber) {
+        this.number = number;
+        this.value = new String(value);
+        words = value.split("\\s+");
+        noOfWords = words.length;
+        score = 0.0;
+        this.paragraphNumber = paragraphNumber;
+
+    }
 }
