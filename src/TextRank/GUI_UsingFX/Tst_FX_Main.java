349763f80c8f32617cc6b8d3ffd38eb699a64fbd
jaf107 <48786629+jaf107@users.noreply.github.com>
2020-10-06 18:44:34 +0600

    Menu page basic layout done -Buttons constructed -> Tool,About,Testing,Back
    Signed-off-by: jaf107 <48786629+jaf107@users.noreply.github.com>

--- src/TextRank/GUI_UsingFX/Tst_FX_Main.java
+++ src/TextRank/GUI_UsingFX/Tst_FX_Main.java
@@ -1,7 +1,12 @@
 package TextRank.GUI_UsingFX;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import static javafx.application.Application.launch;
@@ -13,6 +18,59 @@ public class Tst_FX_Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        window = stage;
+
+        // Start page Scene
+        Pane startPane = new Pane();
+        Label introTitle = new Label("Text Summarization Tool");
+        Button startButton = new Button("Start");
+
+        introTitle.setTranslateX(100);
+        introTitle.setTranslateY(250);
+        startButton.setTranslateX(300);
+        startButton.setTranslateY(250);
+
+        startPane.getChildren().addAll(introTitle,startButton);
+
+        // Menu Scene
+
+        Button toolButton = new Button("Tool");
+        Button aboutButton = new Button("About");
+        Button testButton = new Button("Testing");
+        Button backButton_Menu = new Button("Back");
+
+        Pane menuPane = new Pane();
+
+        // Menu buttons coordinates
+        toolButton.setTranslateX(250);
+        toolButton.setTranslateY(140);
+        aboutButton.setTranslateX(250);
+        aboutButton.setTranslateY(240);
+        testButton.setTranslateX(250);
+        testButton.setTranslateY(340);
+        backButton_Menu.setTranslateX(50);
+        backButton_Menu.setTranslateY(400);
+
+
+
+        menuPane.getChildren().addAll(toolButton,aboutButton,testButton,backButton_Menu);
+        menu = new Scene(menuPane,500,500);
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Im here");
+                window.setScene(menu);
+            }
+        });
+
+        start = new Scene(startPane,500,500);
+
+        window.setTitle("Text Summarizer Tool");
+        window.setScene(start);
+        window.show();
+
+
 
     }
 
